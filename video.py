2c7ab2ef3c164f11069534aa20a45e303cdacf73
zhangliukun <1433317488@qq.com>
2019-01-18 15:42:44 +0800

    添加video

--- video.py
+++ video.py
@@ -73,7 +73,9 @@ def write(x, results):
     c2 = tuple(x[3:5].int())
     img = results
     cls = int(x[-1])
-    color = random.choice(colors)
+    color = random.choice(colors[1:])
+    if cls == 0:
+        color = colors[0]
     label = "{0}".format(classes[cls])
     cv2.rectangle(img, c1, c2, color, 1)
     t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
